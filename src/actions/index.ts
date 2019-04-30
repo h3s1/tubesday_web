0bdb2df3f29ccf10376ef5b3a5ed7313f3f86cf6
공채원 <chaewonkong@gmail.com>
2019-04-30 23:36:05 +0900

    feat: add action creator for youtube search

---
+++ src/actions/index.ts
@@ -0,0 +1,8 @@
+import {ADD_SEARCHED} from "./types"
+
+export const addSearchedResults = (searchedResults: any[]): {type: string, payload: any[]} => {
+    return {
+        type: ADD_SEARCHED,
+        payload: searchedResults
+    }
+}
