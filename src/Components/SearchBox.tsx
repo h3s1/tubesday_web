1bcfab8d45a3ad0fc05cdc7a6234de6bc85a5a92
공채원 <chaewonkong@gmail.com>
2019-04-30 14:02:50 +0900

    feat: add state to SearchBox

--- src/Components/SearchBox.tsx
+++ src/Components/SearchBox.tsx
@@ -1,14 +1,28 @@
 import React from "react"
 import styled from 'styled-components'
 import {Input} from "antd"
+// import axios from 'axios'
 
 const Search = Input.Search;
 
-const SearchBox: React.SFC<{}> = ({}) => (
-    <Container>
-        <Search style={{width: "50vw"}}/>
-    </Container>
-  );
+interface IProps {}
+interface IState { searchText: string}
+
+class SearchBox extends React.Component<IProps, IState> {
+  state = {searchText: ""}
+
+  handleTextInput (e: any) {
+    this.setState({searchText: e.target.value})
+  }
+  render() {
+    console.log(this.state.searchText)
+    return (
+      <Container>
+          <Search style={{width: "50vw"}} onChange={e=>this.handleTextInput(e)}/>
+      </Container>
+    );
+  }
+}
 
 const Container = styled.div``
 
