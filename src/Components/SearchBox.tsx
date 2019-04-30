4ef4e6dd7defe15968f51bcef2fc31ceef377cb5
공채원 <chaewonkong@gmail.com>
2019-04-30 16:51:52 +0900

    feat: add handleSearch()
    fetch results using Youtube data api.

--- src/Components/SearchBox.tsx
+++ src/Components/SearchBox.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from 'styled-components'
 import {Input} from "antd"
-// import axios from 'axios'
+import axios from 'axios'
 
 const Search = Input.Search;
 
@@ -14,11 +14,18 @@ class SearchBox extends React.Component<IProps, IState> {
   handleTextInput (e: any) {
     this.setState({searchText: e.target.value})
   }
+
+  async handleSearch() {
+    const key: string = "AIzaSyCSRYRn55nm_-CWnJV9HxoRLwyMm215X2w"
+    const {searchText} = this.state
+    const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${searchText}&key=${key}`)
+    console.log(res.data.items)
+  }
   render() {
-    console.log(this.state.searchText)
+    console.log(this.state)
     return (
       <Container>
-          <Search style={{width: "50vw"}} onChange={e=>this.handleTextInput(e)}/>
+          <Search style={{width: "50vw"}} onChange={e=>this.handleTextInput(e)} onPressEnter={() => this.handleSearch()}/>
       </Container>
     );
   }
