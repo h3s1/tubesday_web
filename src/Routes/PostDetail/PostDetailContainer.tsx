1ab9f3ed66c10303eef550e9ce00cc9de44c187e
Geon Lee <leegun2003@gmail.com>
2019-05-01 21:30:20 +0900

    feature: post detail video showing

--- src/Routes/PostDetail/PostDetailContainer.tsx
+++ src/Routes/PostDetail/PostDetailContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { PostDetailPresenter } from "./PostDetailPresenter";
+import { postApi } from "../../api";
 
 interface IProps {
   match: {
@@ -9,19 +10,64 @@ interface IProps {
   };
 }
 
-interface IState {}
+interface IState {
+  status: string;
+  data: {
+    no: number;
+    title: string;
+    videoId: string;
+    author: string;
+    content: string;
+    datetime: string;
+  } | null;
+}
 
 export default class PostDetailContainer extends React.Component<
   IProps,
   IState
 > {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      data: null,
+      status: "failure"
+    };
+  }
+  async componentDidMount() {
+    console.log("didmount");
+    try {
+      const {
+        match: {
+          params: { postId }
+        }
+      } = this.props;
+      const parsedId = parseInt(postId);
+      const {
+        data: { data, status }
+      } = await postApi.postDetail(parsedId);
+      console.log(data);
+      this.setState({
+        data,
+        status
+      });
+    } catch (error) {
+      alert(error);
+    } finally {
+    }
+  }
+
   render() {
     const {
       match: {
         params: { postId }
       }
     } = this.props;
-    console.log(this.props);
-    return <PostDetailPresenter id={parseInt(postId)} />;
+    const { data, status } = this.state;
+    return (
+      data &&
+      status == "success" && (
+        <PostDetailPresenter data={data} id={parseInt(postId)} />
+      )
+    );
   }
 }
--- src/Routes/PostDetail/PostDetailPresenter.tsx
+++ src/Routes/PostDetail/PostDetailPresenter.tsx
@@ -1,9 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+import ReactPlayer from "react-player";
+
+const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
 
 interface IProps {
   id: number;
+  data: {
+    no: number;
+    title: string;
+    videoId: string;
+    author: string;
+    content: string;
+    datetime: string;
+  };
 }
 
-export const PostDetailPresenter: React.SFC<IProps> = ({ id }) => (
-  <div>Post Detail of postId {id}</div>
+export const PostDetailPresenter: React.SFC<IProps> = ({ data, id }) => (
+  <Container>
+    <ReactPlayer
+      url={`https://www.youtube.com/watch?v=${data.videoId}`}
+      playing={true}
+      loop={true}
+      controls={false}
+      width="100%"
+      height="100%"
+      onReady={() => console.log("video ready")}
+      onError={() => console.log("video error")}
+      style={{
+        transition: "3s ease-in-out",
+        overflow: "hidden",
+        position: "absolute",
+        zIndex: -1
+      }}
+    />
+    Post Detail of postId {id}
+  </Container>
 );
