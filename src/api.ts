6bf4bc7545d1788f41c80563a6c39f703b0d0d07
Geon Lee <leegun2003@gmail.com>
2019-07-07 15:52:32 +0900

    ✨ article recommendations in article detail

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ISimplePost, IPost, IComment, Category } from './shared-interfaces';
+import { ISimplePost,  IComment, Category, IPost } from './shared-interfaces';
 
 const baseApi = axios.create({
   baseURL: "https://youtupia-dev.herokuapp.com/"
@@ -9,8 +9,9 @@ const baseApi = axios.create({
 export const articleApi = {
   getArticles: (category: Category, pageNumber: number) => baseApi.get<ISimplePost[]>(`articles`, { params: {category, pageNumber}}),
   getArticle: (article_id: number) => 
-  axios.all<any>([
+    axios.all<any>([
     baseApi.get<IPost>(`articles/${article_id}`),
     baseApi.get<IComment[]>(`articles/${article_id}/comments`),
+    baseApi.get<ISimplePost[]>(`recommendations`)
   ]),
 };
